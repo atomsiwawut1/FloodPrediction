f01747c313c4b2b6dee80f2fb21d42b51f4e2ae9
“atomsiwawut1 <atomsiwawut@gmail.com>
2022-11-25 12:58:17 +0700

    “Update

--- pages/1_home.py
+++ pages/1_home.py
@@ -13,4 +13,11 @@ st.set_page_config(page_title='Home',page_icon="🧊",layout='centered')
 
 st.header('My Hobby')
 image_url="https://raw.githubusercontent.com/atomsiwawut1/FloodPrediction/main/Media/hunt.jpg"
-st.image(image_url,caption="he best survival game",width=500)
+st.image(image_url,caption="best survival game",width=500)
+
+st.header('ปรับปรุงริมน้ำตลาดท่านา')
+st.image("Media\TANA.png",caption="best survival game",width=500)
+
+
+st.header('ปรับปรุงริมน้ำตลาดท่านา')
+st.image("Media\TANA2.png",caption="best survival game",width=500)
